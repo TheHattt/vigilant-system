5f6c47e5193c7391378749d280cebf070eba31bc
talyx <ngwemoa@gmail.com>
2026-01-20 20:06:05 +0300

    ci: fix test workflow by adding database preparation and global test tenant

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,47 +1,65 @@
 <?php
 
+use App\Models\Tenant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
 |--------------------------------------------------------------------------
 |
 | The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "pest()" function to bind a different classes or traits.
+| case class. By default, that class is "PHPUnit\Framework\TestCase".
 |
 */
 
-pest()->extend(Tests\TestCase::class)
-    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+pest()
+    ->extend(TestCase::class)
+    ->use(RefreshDatabase::class)
+    ->beforeEach(function () {
+        // Create a default tenant for every test to prevent Foreign Key errors
+        // This acts as the "Standard ISP" for your staff login tests
+        Tenant::firstOrCreate(
+            ["account_prefix" => "TST"], // Use 'prefix' to match your migration
+            [
+                "name" => "Test ISP Company",
+                "slug" => "test-isp-company",
+            ],
+        );
+    })
+    ->in("Feature", "Unit");
 
 /*
 |--------------------------------------------------------------------------
 | Expectations
 |--------------------------------------------------------------------------
 |
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
+| The "expect()" function gives you access to a set of "expectations" methods
+| that you can use to assert different things.
 |
 */
 
-expect()->extend('toBeOne', function () {
+expect()->extend("toBeOne", function () {
     return $this->toBe(1);
 });
 
 /*
 |--------------------------------------------------------------------------
-| Functions
+| Helpers
 |--------------------------------------------------------------------------
 |
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
+| Expose helpers as global functions to help you reduce the number of
+| lines of code in your test files.
 |
 */
 
-function something()
+function actingAsStaff()
 {
-    // ..
+    $tenant = Tenant::where("account_prefix", "TST")->first();
+    $user = \App\Models\User::factory()->create([
+        "tenant_id" => $tenant->id,
+    ]);
+
+    return test()->actingAs($user);
 }
